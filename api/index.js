4d28ab2eefe179a348a20046bb322caa0c294c78
Pochana Srikar Reddy <pochasri@amazon.com>
2024-08-18 18:50:18 +0530

    First NodeJS deployment

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
 
 mongoose
@@ -21,19 +21,24 @@ mongoose
     console.log(err);
   });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
 
-app.use("/api/user", userRoutes); 
+app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server error';
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+app.get("/test", (req, res) => {
+  res.send("Hello NodeJS");
 });
