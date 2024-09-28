5b652462cac4666ddfedccebd14192b8d14aaacd
Pochana Srikar Reddy <pochasri@amazon.com>
2024-09-28 19:20:26 +0530

    Add pre installation for deployment

--- api/index.js
+++ api/index.js
@@ -5,8 +5,11 @@ import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -38,4 +41,10 @@ app.use((err, req, res, next) => {
         statusCode,
         message
     });
+});
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
